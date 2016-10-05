e63fa63f98e137ff1ac531c2684b5d4506ade689
Mikela Clemmons <glassresistor@gmail.com>
2016-10-05 13:21:19 -0700

    removed static dirs

--- san_diego_resources/settings/development.py
+++ san_diego_resources/settings/development.py
@@ -12,8 +12,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '../staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '../static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
