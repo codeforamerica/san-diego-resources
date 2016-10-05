fe1d49034954f541ca4ca0234ab660612843da28
Mikela Clemmons <glassresistor@gmail.com>
2016-10-05 13:10:31 -0700

    added runtime and more heroku requirements

--- san_diego_resources/settings/development.py
+++ san_diego_resources/settings/development.py
@@ -1,4 +1,18 @@
 import dj_database_url
-from san_diego_resources.settings.environment import *
+import os
+from san_diego_resources.settings.base import *
 
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
