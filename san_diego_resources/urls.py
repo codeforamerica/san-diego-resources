ca39f1cc1ee69cecb30e31ca5bc0146519f17ac0
Mikela Clemmons <glassresistor@gmail.com>
2016-11-28 14:26:58 -0800

    added frontend urls

--- san_diego_resources/urls.py
+++ san_diego_resources/urls.py
@@ -4,25 +4,8 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'', include('wealthmap.frontend_urls', namespace='wealthmap')),
     url(r'^admin/', admin.site.urls),
     url(r'^wm-api/', include('wealthmap.urls', namespace='wealthmap-api')),
 ]
